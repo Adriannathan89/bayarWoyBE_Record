ad1631f339fbb1a13ecf40eee8404edc1726d1a3
Adrian Nathanael <adrianpersonal677@gmail.com>
2026-04-23 15:10:39 +0700

    retry: ci retry

--- config/database.go
+++ config/database.go
@@ -11,14 +11,14 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	host:= GetEnv("DB_HOST")
-	port:= GetEnv("DB_PORT")
-	user:= GetEnv("DB_USER")
-	password:= GetEnv("DB_PASSWORD")
-	name:= GetEnv("DB_NAME")
+	host := GetEnv("DB_HOST")
+	port := GetEnv("DB_PORT")
+	user := GetEnv("DB_USER")
+	password := GetEnv("DB_PASSWORD")
+	name := GetEnv("DB_NAME")
 
-	connection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
-	host, port, user, password, name)
+	connection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, name)
 
 	db, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
 	if err != nil {
