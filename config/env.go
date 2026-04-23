55391482c30dcd32566fee77e4f35e63c215b557
Adrian Nathanael <adrianpersonal677@gmail.com>
2026-04-23 14:52:18 +0700

    fix: fixing dockerfile

--- config/env.go
+++ config/env.go
@@ -3,15 +3,20 @@ package config
 import (
 	"log"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 )
 
+var loadEnvOnce sync.Once
+
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
+	loadEnvOnce.Do(func() {
+		err := godotenv.Load()
+		if err != nil && !os.IsNotExist(err) {
+			log.Printf("Error loading .env file: %v", err)
+		}
+	})
 }
 
 func GetEnv(key string) string {
